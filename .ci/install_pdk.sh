1c7e446c2db54460c9ff56e5f737d37e027d5d76
Tobias Knipping <knipping@tk-schulsoftware.de>
2023-05-30 15:40:00 +0200

    fix test workflow

---
+++ .ci/install_pdk.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x # echo commands with vars expanded
+set -e # exit immediately on error
+
+DIST_NAME=$(lsb_release -cs)
+RELEASE_DEB="https://apt.puppetlabs.com/puppet-tools-release-${DIST_NAME}.deb"
+NIGHTLY_DEB="https://nightlies.puppetlabs.com/apt/puppet6-nightly-release-${DIST_NAME}.deb"
+
+setup_apt() {
+    local deb_url="${1}"
+    local deb_file=$(basename "${deb_url}")
+
+    wget "${deb_url}"
+    sudo dpkg -i "${deb_file}"
+}
+
+main() {
+    if [ -z "${PDK}" -o "${PDK}" = "release" ]; then
+        setup_apt "${RELEASE_DEB}"
+    elif [ "${PDK}" = "nightly" ]; then
+        setup_apt "${NIGHTLY_DEB}"
+    else
+        echo "Unknown \$PDK value '${PDK}'. Supported values are 'release' and 'nightly'." >&2
+        exit 1
+    fi
+
+    sudo apt-get update -qq
+    sudo apt-get install -y pdk
+
+    /usr/local/bin/pdk --version
+}
+
+main "$@"
---
+++ .ci/test_script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -x # echo commands with vars expanded
+set -e # exit immediately on error
+
+TEMPLATE_PR_DIR=$PWD
+
+# Make a branch from checked out HEAD so that we can target
+# it specifically with --template-ref
+git checkout -b ci_commit
+
+# Test if new module from PR commit is still functional.
+pdk new module new_module --template-url="file://$TEMPLATE_PR_DIR" --template-ref=ci_commit --skip-interview
+pushd new_module
+grep template < metadata.json
+cp "$TEMPLATE_PR_DIR/.ci/fixtures/new_provider_sync.yml" ./.sync.yml
+pdk update --force
+pdk new class new_module
+pdk new defined_type test_type
+pdk new fact test_fact || true # not available in pdk 1.18 yet
+pdk new function --type native testfunc_nat || true # not available in pdk 1.18 yet
+pdk new function --type v4 testfunc_v4 || true # not available in pdk 1.18 yet
+pdk new provider test_provider
+pdk new task test_task
+pdk new transport test_transport
+pdk validate
+pdk test unit
+popd
+
+rm -f ~/.pdk/cache/answers.json
+
+# Create new module from default template-url and release tag
+pdk new module convert_from_release_tag --skip-interview
+pushd convert_from_release_tag
+grep template < metadata.json
+# Attempt to convert to PR commit from release tag
+pdk convert --template-url="file://$TEMPLATE_PR_DIR" --template-ref=ci_commit --skip-interview --force
+cat convert_report.txt
+popd
+
+rm -f ~/.pdk/cache/answers.json
+
+# Create new module from main branch of official templates repo
+pdk new module convert_from_main --template-url="https://github.com/puppetlabs/pdk-templates.git" --template-ref=main --skip-interview
+pushd convert_from_main
+grep template < metadata.json
+# Attempt to convert to PR commit from official/main
+pdk convert --template-url="file://$TEMPLATE_PR_DIR" --template-ref=ci_commit --skip-interview --force
+cat convert_report.txt
+popd
